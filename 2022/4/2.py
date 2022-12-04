0ef8a2fb3392486525e768a0ee8ce8f575ce6093
Janick Braun <57684270+janickbraun@users.noreply.github.com>
2022-12-04 14:21:04 +0100

    2022 day 4

---
+++ 2022/4/2.py
@@ -0,0 +1,26 @@
+with open("data.txt") as f:
+    lines = f.read().splitlines()
+
+pairsOld = []
+
+for line in lines:
+    pairsOld.append(line.split(","))
+
+pairs = []
+
+for pair in pairsOld:
+    par = []
+    for elf in pair:
+        par.append(elf.split("-"))
+    pairs.append(par)
+
+count = 0
+
+for pair in pairs:
+    pair = list(map(int, pair[0])), list(map(int, pair[1]))
+
+    if pair[0][1] >= pair[1][0] and pair[1][1] >= pair[0][0]:
+        print(pair)
+        count += 1
+
+print(count)
