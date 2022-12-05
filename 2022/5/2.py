33ed097e5f6a4eaae4dbea4e32689616de3166b2
Janick Braun <57684270+janickbraun@users.noreply.github.com>
2022-12-05 20:52:46 +0100

    day 5 2022

---
+++ 2022/5/2.py
@@ -0,0 +1,43 @@
+import re
+
+with open("data.txt") as f:
+    lines = f.read().splitlines()
+
+chests = []
+moves = []
+box = []
+
+for line in lines:
+    if line.startswith("move"):
+        moves.append(list(map(int, re.findall(r'\d+', line))))
+
+def goHigher(lineIndex, letterIndex):
+    try:
+        if lines[lineIndex - 1][letterIndex] != " " and lines[lineIndex - 1][letterIndex].isnumeric() == False and lines[lineIndex - 1][letterIndex] != "o":
+            box.append(lines[lineIndex - 1][letterIndex])
+            goHigher(lineIndex - 1, letterIndex)
+    except IndexError:
+        pass
+
+for lineIndex, line in enumerate(lines):
+    if line.startswith(" 1"):
+        for letterIndex, letter in enumerate(line):
+            if letter != " ":
+                goHigher(lineIndex, letterIndex)
+                chests.append(box)
+                box = []
+
+for chest in chests:
+    chest = chest.reverse()
+
+for move in moves:
+    upe = chests[move[1] - 1][0:move[0]]
+    for i in range(move[0]):
+        chests[move[1] - 1].pop(0)
+    chests[move[2] - 1] = upe + chests[move[2] - 1]
+
+print(chests)
+print(moves)
+
+for chest in chests:
+    print(chest[0], end="")
